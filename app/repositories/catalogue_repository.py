8d98eff9f3fe96c02f522e8f0af29a2ff893a82f
icondor2019 <ivalrxis14@gmail.com>
2024-06-25 20:41:19 -0500

    <ADD: scraping catalogue logic>

---
+++ app/repositories/catalogue_repository.py
@@ -0,0 +1,36 @@
+from loguru import logger
+from sqlalchemy import text
+from configuration.database import db
+from sqlalchemy.exc import SQLAlchemyError
+from responses.catalogue_response import CatalogueResponse
+from repositories.queries.catalogue_query import (
+    CREATE_PRODUCT_QUERY,
+    VECTOR_SEARCH_QUERY
+)
+
+
+class CatalogueRepository:
+    """Manage Catalogue records & embeddings"""
+
+    def add_new_product(self, product_record: CatalogueResponse):
+        try:
+            params = product_record.model_copy().model_dump()
+            db.execute(text(CREATE_PRODUCT_QUERY), params)
+            db.commit()
+            logger.debug(f"New product created. uuid: {params['uuid']}")
+        except SQLAlchemyError as ex:
+            db.rollback()
+            logger.error(f"Error saving seller. uuid {params['uuid']}. Error: {ex}")
+
+    def get_similar_product(self, vector: str):
+        logger.debug(VECTOR_SEARCH_QUERY)
+        result = None
+        try:
+            params = {'embedding': vector}
+            result = db.execute(text(VECTOR_SEARCH_QUERY), params).fetchone()
+            logger.debug(result)
+            db.commit()
+        except SQLAlchemyError as ex:
+            db.rollback()
+            logger.error(f"Error consulting vecto. Error: {ex}")
+        return result
---
+++ app/responses/catalogue_response.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+
+class CatalogueResponse(BaseModel):
+    uuid: str = Field(...)
+    name: Optional[str] = Field(None)
+    size: int = Field(...)
+    color: Optional[str] = Field(None)
+    stems: int = Field(...)
+    package: str = Field(default='HB')
+    description: Optional[str] = Field(None)
+    embedding: str = Field(...)
+
+    class Config:
+        orm_mode = True
